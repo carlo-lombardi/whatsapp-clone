432d04fac2c656550da7fdf79738ee5d6ed0d6ef
Carlo Lombardi <carlo_lombardi@users.noreply.github.com>
2021-09-28 09:17:50 +0100

    Icon GrChat and MoreVertical was added

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import { GrChat } from "react-icons/Gr";
+import { GiBalaclava } from "react-icons/Gi";
+import { FiMoreVertical } from "react-icons/Fi";
+
 function Sidebar() {
   return (
     <Container>
       <Header>
-        <GrChat />
-        <IconsContainer></IconsContainer>
+        <UserAvatar />
+        <IconsContainer>
+          <GrChat />
+          <FiMoreVertical />
+        </IconsContainer>
       </Header>
     </Container>
   );
@@ -16,5 +22,7 @@ export default Sidebar;
 const Container = styled.div``;
 
 const Header = styled.div``;
+
+const UserAvatar = styled(GiBalaclava)``;
 
 const IconsContainer = styled.div``;
